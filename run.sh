7a7d5e1bc72e1e5435b65d5e6b13908e06d62c78
zemiak <zemiak@gmail.com>
2019-11-17 09:28:12 +0100

    volume fix

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker rm -f tiny-maven-proxy
-docker run --name=tiny-maven-proxy -d -v /var/lib/maven:/mnt/media/maven -p 5956:5956 tiny-maven-proxy
+docker run --name=tiny-maven-proxy -d -v /mnt/media/maven:/var/lib/maven -p 5956:5956 tiny-maven-proxy
